c6dfbeff4e5c63c0aa5bd16e07adbbb0bf857591
Aracelli La Rosa <a20206456@pucp.edu.pe>
2024-04-24 19:28:10 -0500

    Implementando Controllers

---
+++ LAB4_20206456/src/main/java/com/example/lab4_20206456/controller/ReporteController.java
@@ -0,0 +1,15 @@
+package com.example.lab4_20206456.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reportes")
+public class ReporteController {
+    @GetMapping(value = {""})
+    public String verReportes(){
+        return "reportes";
+    }
+    
+}
---
+++ LAB4_20206456/src/main/java/com/example/lab4_20206456/repository/EmpleadoRepository.java
@@ -0,0 +1,4 @@
+package com.example.lab4_20206456.repository;
+
+public interface EmpleadoRepository {
+}
