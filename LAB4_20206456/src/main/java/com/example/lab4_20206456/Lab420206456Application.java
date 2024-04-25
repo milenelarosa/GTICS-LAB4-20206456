064a3c1e0ac83a9677f805d9eeb96426ce832be8
Aracelli La Rosa <a20206456@pucp.edu.pe>
2024-04-24 19:18:43 -0500

    Subiendo proyecto

---
+++ LAB4_20206456/src/main/java/com/example/lab4_20206456/Lab420206456Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20206456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420206456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420206456Application.class, args);
+	}
+
+}
---
+++ LAB4_20206456/src/test/java/com/example/lab4_20206456/Lab420206456ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20206456;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420206456ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
