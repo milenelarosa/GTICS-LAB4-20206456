56a8ca252082fe9ccfee3927632854e9c6781952
Aracelli La Rosa <a20206456@pucp.edu.pe>
2024-04-24 20:34:14 -0500

    Lista empleados

--- LAB4_20206456/src/main/java/com/example/lab4_20206456/controller/EmpleadosController.java
+++ LAB4_20206456/src/main/java/com/example/lab4_20206456/controller/EmpleadosController.java
@@ -1,14 +1,28 @@
 package com.example.lab4_20206456.controller;
 
+import com.example.lab4_20206456.entity.Empleados;
+import com.example.lab4_20206456.repository.EmpleadoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/empleados")
 public class EmpleadosController {
+
+    final EmpleadoRepository empleadoRepository;
+
+    public EmpleadosController(EmpleadoRepository empleadoRepository) {
+        this.empleadoRepository = empleadoRepository;
+    }
+
     @GetMapping(value = {""})
-    public String listarEmpleados(){
+    public String listarEmpleados(Model model){
+        List<Empleados> empleadosList = empleadoRepository.listarEmpleados();
+        model.addAttribute("listaEmpleados", empleadosList);
         return "empleados";
     }
 
--- LAB4_20206456/src/main/java/com/example/lab4_20206456/repository/EmpleadoRepository.java
+++ LAB4_20206456/src/main/java/com/example/lab4_20206456/repository/EmpleadoRepository.java
@@ -1,4 +1,14 @@
 package com.example.lab4_20206456.repository;
 
-public interface EmpleadoRepository {
+import com.example.lab4_20206456.entity.Empleados;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface EmpleadoRepository extends JpaRepository<Empleados,Integer> {
+
+    @Query(nativeQuery = true, value = "SELECT e.*, j.job_title, d.department_name, l.city FROM hr.employees e, hr.departments d, hr.jobs j, hr.locations l\n" +
+            "WHERE e.department_id = d.department_id and e.job_id = j.job_id and d.location_id = l.location_id;\n")
+    List<Empleados> listarEmpleados();
 }
